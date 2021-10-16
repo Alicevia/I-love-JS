d4284219837c3fdd09ac2bca8828b4e424724416
zhangchengfei <ed-phoenix@qq.com>
2021-10-16 23:22:30 +0800

    'compose'

--- koa/index.js
+++ koa/index.js
@@ -1,8 +1,13 @@
 import Koa from "./lib/application.js";
 
 let app = new Koa();
+app.use(async (ctx, next) => {
+  console.log(1);
+  ctx.body = "rl";
+  await next();
+});
 app.use((ctx) => {
-  ctx.body = "helloworld";
+  console.log(3);
 });
 app.on("error", (err) => {
   console.log(err);
--- koa/lib/application.js
+++ koa/lib/application.js
@@ -8,15 +8,15 @@ class Application extends EventEmitter {
   context;
   response;
   request;
-  callback;
+  middlewares = [];
   constructor() {
     super();
     this.request = new Request();
     this.response = new Response();
     this.context = new Context(this.request, this.response);
   }
-  use(callback) {
-    this.callback = callback;
+  use(middleware) {
+    this.middlewares.push(middleware);
   }
   createContext(req, res) {
     let response = new Response(res);
@@ -24,16 +24,37 @@ class Application extends EventEmitter {
     let context = new Context(request, response);
     return context;
   }
+  compose(ctx) {
+    let index = -1;
+    const dispatch = (i) => {
+      if (index >= i) {
+        return Promise.reject("next() call multiples times");
+      }
+      index = i;
+      if (this.middlewares.length === i) return Promise.resolve();
+      let middleware = this.middlewares[i];
+      try {
+        return Promise.resolve(middleware(ctx, () => dispatch(i + 1)));
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    };
+    return dispatch(0);
+  }
   handleRequest = (req, res) => {
     let context = this.createContext(req, res);
     res.statusCode = 404;
-    this.callback(context);
-    console.log(context.body);
-    if (context.body) {
-      res.end(context.body);
-    } else {
-      res.end("Not Found");
-    }
+    this.compose(context)
+      .then(() => {
+        if (context.body) {
+          res.end(context.body);
+        } else {
+          res.end("Not Found");
+        }
+      })
+      .catch((e) => {
+        this.emit("error", e);
+      });
   };
   listen(...args) {
     let server = http.createServer(this.handleRequest);
